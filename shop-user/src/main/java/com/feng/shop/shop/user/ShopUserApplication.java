4a46f054f700a4c57a6988616b5fc7bd4d85c803
feng <453489252@qq.com>
2020-03-18 15:55:55 +0800

    新建user项目

---
+++ shop-user/src/main/java/com/feng/shop/shop/user/ShopUserApplication.java
@@ -0,0 +1,13 @@
+package com.feng.shop.shop.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopUserApplication.class, args);
+    }
+
+}
